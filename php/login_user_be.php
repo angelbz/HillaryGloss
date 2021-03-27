05367415e04d6aecf5f042b60caabaa3c519d4a6
angelbz <81385504+angelbz@users.noreply.github.com>
2021-03-27 13:44:55 -0600

    Update login_user_be.php

--- php/login_user_be.php
+++ php/login_user_be.php
@@ -7,13 +7,13 @@
 
     $verify_login = mysqli_query($conect, "SELECT * FROM users WHERE user='$usuario' AND pwd='$password'");
     if(mysqli_num_rows($verify_login) > 0){
-       header("location:../inicio.html");
+       header("location:..inicio.html");
        exit;
     }else{   
         echo '
         <script>
             alert("Usuario no existe, por favor verifique sus datos");
-            window.location = "../index.html";
+            window.location = "..index.html";
         </script>
         ';
         exit;
